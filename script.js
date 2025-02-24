fac31e27bae3998b61c43f7f3f2ae31935148ca7
Fxdzie <manwafadziso@gmail.com>
2025-02-24 10:29:53 +0100

    Addition of playRound function

--- script.js
+++ script.js
@@ -17,3 +17,47 @@ function getHumanChoice(){
         }
     return choice;
 }
+
+function playGame(){
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice,computerChoice){
+        
+        switch(humanChoice){
+            case "rock":
+                if(computerChoice === "scissors"){
+                    console.log("You win! Rock beats Scissors");
+                    humanScore++;
+                }else if(computerChoice==="paper"){
+                    console.log("You lose! Paper beats Rock");
+                    computerScore++;
+                }else{
+                    console.log("Interesting. Looks like its a tie.");
+                }
+                break;
+            case "paper":
+                if(computerChoice === "scissors"){
+                    console.log("You lose! Scissors beats Paper");
+                    computerScore++;
+                }else if(computerChoice==="paper"){
+                    console.log("Interesting. Looks like its a tie.");
+                }else{
+                    console.log("You win! Paper beats Rock");
+                    humanScore++;
+                }
+                break;
+            case "scissors":
+                if(computerChoice === "scissors"){
+                    console.log("Interesting. Looks like its a tie.");
+                }else if(computerChoice==="paper"){
+                    console.log("You win! Scissors beats Paper");
+                    humanScore++;
+                }else{
+                    console.log("You lose! Rock beats Scissors");
+                    computerScore++;
+                }
+                break;
+        }
+    }
+}
