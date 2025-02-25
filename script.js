b42a7fea36073e1c9804ed35fa9b7406edf8b70e
Fxdzie <manwafadziso@gmail.com>
2025-02-25 12:02:48 +0100

    Addition of button functionality and showcase of results

--- script.js
+++ script.js
@@ -1,3 +1,54 @@
+const rockButton = document.querySelector("#rock");
+const paperButton = document.querySelector("#paper");
+const scissorsButton = document.querySelector("#scissors");
+const resultsText = document.querySelector(".results");
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice,computerChoice){
+        
+    switch(humanChoice){
+        case "rock":
+            if(computerChoice === "scissors"){
+                resultsText.innerText = "You win! Rock beats Scissors";
+                humanScore++;
+            }else if(computerChoice==="paper"){
+                resultsText.innerText = "You lose! Paper beats Rock";
+                computerScore++;
+            }else{
+                resultsText.innerText = "Interesting. Looks like its a tie.";
+            }
+            break;
+        case "paper":
+            if(computerChoice === "scissors"){
+                resultsText.innerText = "You lose! Scissors beats Paper";
+                computerScore++;
+            }else if(computerChoice==="paper"){
+                resultsText.innerText = "Interesting. Looks like its a tie.";
+            }else{
+                resultsText.innerText = "You win! Paper beats Rock";
+                humanScore++;
+            }
+            break;
+        case "scissors":
+            if(computerChoice === "scissors"){
+                resultsText.innerText = "Interesting. Looks like its a tie.";
+            }else if(computerChoice==="paper"){
+                resultsText.innerText = "You win! Scissors beats Paper";
+                humanScore++;
+            }else{
+                resultsText.innerText = "You lose! Rock beats Scissors";
+                computerScore++;
+            }
+            break;
+    }
+}
+
+rockButton.addEventListener("click",()=>{playRound("rock",getComputerChoice())});
+paperButton.addEventListener("click",()=>{playRound("paper",getComputerChoice())});
+scissorsButton.addEventListener("click",()=>{playRound("scissors",getComputerChoice())});
+
 function getComputerChoice(){
     const choice = Math.floor(Math.random()*3);
     if(choice===0){
@@ -19,57 +70,15 @@ function getHumanChoice(){
 }
 
 function playGame(){
-    let humanScore = 0;
-    let computerScore = 0;
-
-    function playRound(humanChoice,computerChoice){
-        
-        switch(humanChoice){
-            case "rock":
-                if(computerChoice === "scissors"){
-                    console.log("You win! Rock beats Scissors");
-                    humanScore++;
-                }else if(computerChoice==="paper"){
-                    console.log("You lose! Paper beats Rock");
-                    computerScore++;
-                }else{
-                    console.log("Interesting. Looks like its a tie.");
-                }
-                break;
-            case "paper":
-                if(computerChoice === "scissors"){
-                    console.log("You lose! Scissors beats Paper");
-                    computerScore++;
-                }else if(computerChoice==="paper"){
-                    console.log("Interesting. Looks like its a tie.");
-                }else{
-                    console.log("You win! Paper beats Rock");
-                    humanScore++;
-                }
-                break;
-            case "scissors":
-                if(computerChoice === "scissors"){
-                    console.log("Interesting. Looks like its a tie.");
-                }else if(computerChoice==="paper"){
-                    console.log("You win! Scissors beats Paper");
-                    humanScore++;
-                }else{
-                    console.log("You lose! Rock beats Scissors");
-                    computerScore++;
-                }
-                break;
-        }
-    }
-    for(let i=0;i<5;i++){
-        playRound(getHumanChoice(),getComputerChoice());
-    }
-    if(humanScore<computerScore){
-        alert("Sorry!! You have lost the game");
-    }else if(humanScore>computerScore){
-        alert("Congratulations!! You have won the game");
-    }else{
-        alert("Well...Looks like a tie");
-    }
+    
+    
+    // if(humanScore<computerScore){
+    //     alert("Sorry!! You have lost the game");
+    // }else if(humanScore>computerScore){
+    //     alert("Congratulations!! You have won the game");
+    // }else{
+    //     alert("Well...Looks like a tie");
+    // }
 }
 
-playGame();
+
